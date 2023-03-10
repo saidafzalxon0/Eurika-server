76ada4dbe90d250df2268e7b91ba6d39e9578d2e
Saidafzalkhon <Saidafzalkhon.developer@gmail.com>
2023-03-11 00:44:50 +0500

    first commit

---
+++ src/test/java/uz/java/Eurikaserver/EurikaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.java.Eurikaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurikaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
